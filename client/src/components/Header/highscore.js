754333ce6e6256846fe2d72db7054d48c4dbf1ce
Jordan Chadsey <jordan@identipop.com>
2018-01-11 23:56:58 -0800

    Updates

--- client/src/components/Header/highscore.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const HighScore = props => (
-	<span className="scores">{props.highScore}</span>
-);
-
-export default HighScore;
--- client/src/components/Header/score.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Score = props => (
-	<span className="scores">{props.score}</span>
-);
-
-export default Score;
